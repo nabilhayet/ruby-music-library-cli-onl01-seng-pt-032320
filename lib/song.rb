b07f5588cec5583bb797609f68a8ddf5841454c4
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-04 07:47:59 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   extend Concerns::Findable
 attr_accessor :name, :artist, :genre
@@ -35,13 +36,17 @@ attr_accessor :name, :artist, :genre
     artist.add_song(self)
   end
 
-  def self.find_or_create_by_name(name)
-    song=find_by_name(name)
-    if song
-      song
-    else
-      create(name)
-  end
+  def self.new_from_filename(file_name)
+      artist,song,genre = name.split(" - ")
+      genre.chomp(".mp3")
+      binding.pry
+      artist = Artist.find_or_create_by_name(artist)
+
+      genre = Genre.find_or_create_by_name(genre)
+      new(song,artist,genre)
 end
 
+  def self.create_from_filename(name)
+    new_from_filename(name).save
+  end
 end

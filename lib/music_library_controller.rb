22804bbed25599e686155e9634e6fdcf2131d108
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-06 03:43:23 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,9 +18,24 @@ def call
   puts "To quit, type 'exit'."
   puts "What would you like to do?"
 
-  input = gets.strip
+  case input
+            when "list songs"
+              list_songs
+            when "list artists"
+              list_artists
+            when "list genres"
+              list_genres
+            when "list artist"
+              list_songs_by_artist
+            when "list genre"
+              list_songs_by_genre
+            when "play song"
+              play_song
+            end
+
+        input = gets.strip
   end
-  end
+end
 
   def list_songs
       Song.all.sort{|a,b| a.name<=>b.name}.each.with_index(1) do |song,idx|
@@ -69,10 +84,13 @@ end
   def play_song
     puts 'Which song number would you like to play?'
     input = gets.strip.to_i
-    if(1..Song.all.length).include?(input)
+      if(1..Song.all.length).include?(input)
       song = Song.all.sort{|a,b| a.name<=>b.name}[input -1]
+        if song
+          puts "Playing #{song.name} by #{song.artist.name}"
+        end
+      end
     end
-    puts "Playing #{song.name} by #{song.artist.name}"
   end
-end
+
 end
